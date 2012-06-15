6fe34a4f95036a8e7857651d6df98f9fec9da54d
Ethan <ethan@haut.com>
2012-06-15 16:09:58 +0800

    finance manage

--- app/controllers/finance/expense_applications_controller.rb
+++ app/controllers/finance/expense_applications_controller.rb
@@ -2,4 +2,12 @@ class Finance::ExpenseApplicationsController < Finance::BaseController
   def index
     @expense_applications = ExpenseApplication.all
   end
+
+  def show
+  	@expense_application = ExpenseApplication.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @expense_application }
+    end
+  end
 end
