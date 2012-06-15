2ab37c3ef6d031bc3d3fadb1029f32a9e1a4b349
liuqingbo <lqb198612@126.com>
2012-06-15 15:51:03 +0800

    fix heroku bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,13 +16,13 @@ class User < ActiveRecord::Base
   has_many :message_receivers
   has_many :receive_messages, :through => :message_receivers do
     def communicate_messages
-      where('messages.type == ?', "CommunicateMessage")
+      where('messages.type = ?', "CommunicateMessage")
     end
     def prompt_messages
-      where('messages.type == ?', "PromptMessage")
+      where('messages.type = ?', "PromptMessage")
     end
     def unread
-      where('message_receivers.state == ?', "unread")
+      where('message_receivers.state = ?', "unread")
     end
   end
 
