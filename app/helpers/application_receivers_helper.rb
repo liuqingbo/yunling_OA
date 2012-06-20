7acb7b13ab1951555286e614f750d864929bcdbd
liuqingbo <lqb198612@126.com>
2012-06-20 16:00:21 +0800

    fix some I18n bug

--- app/helpers/application_receivers_helper.rb
+++ app/helpers/application_receivers_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationReceiversHelper
   def state_for_select
     result = []
-    result << [I18n.t('views_application_receivers.pending'), 'pending']
-    result << [I18n.t('views_application_receivers.approved'), 'approved']
-    result << [I18n.t('views_application_receivers.rejected'), 'rejected']
+    result << [I18n.t('activerecord.state.application.pending'), 'pending']
+    result << [I18n.t('activerecord.state.application.approved'), 'approved']
+    result << [I18n.t('activerecord.state.application.rejected'), 'rejected']
     result
   end
 end
