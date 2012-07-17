5fa0c2350b320bfe2e37b82ab5fb3b0ef52f9176
liuqingbo <lqb198612@126.com>
2012-07-17 09:44:06 +0800

    delete bug log at Finance::BaseController which will cause error at 114 server

--- app/controllers/finance/base_controller.rb
+++ app/controllers/finance/base_controller.rb
@@ -2,9 +2,6 @@ class Finance::BaseController < ApplicationController
   before_filter :check_finance_or_not
   protected
     def check_finance_or_not
-      p 'ddddddd'
-      p Role::ROLES[:finance]
-      p current_user.roles_name
       unless current_user && current_user.roles_name.include?(Role::ROLES[:finance])
         redirect_to root_path,
                       :notice=>I18n.t("error.permit_deny")
